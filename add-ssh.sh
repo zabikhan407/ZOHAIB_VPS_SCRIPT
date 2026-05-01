a277ad090ec92c3bdc5e8c913692dcfa1bdd02f9
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 01:22:37 +0500

    Update add-ssh.sh

--- add-ssh.sh
+++ add-ssh.sh
@@ -2,10 +2,15 @@
 clear
 read -p "Username : " user
 read -p "Password : " pass
+read -p "Limit IP Login : " iplimit
 read -p "Duration (Days) : " days
+
 exp=`date -d "$days days" +"%Y-%m-%d"`
 useradd -e $exp -M -s /bin/false $user
 echo "$user:$pass" | chpasswd
-echo "SSH Account $user Created! Expires: $exp"
-sleep 3
-ssh-menu
+
+# IP Limit Store karna
+echo "$user $iplimit" >> /etc/zohaib/limit-ssh.db
+
+echo -e "SSH WS Created!\nHost: $(curl -s ipinfo.io/ip)\nUser: $user\nPass: $pass\nIP Limit: $iplimit"
+sleep 5 ; ssh-menu
