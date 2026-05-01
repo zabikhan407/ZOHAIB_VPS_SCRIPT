fcca82a496ef98cf3bb5bef379a75cbb9b3347d4
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 00:50:14 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+# ZOHAIB_NETWORK Professional Setup
+P='\033[0;35m'
+C='\033[0;36m'
+G='\033[0;32m'
+NC='\033[0m'
+
 REPO_URL="https://raw.githubusercontent.com/zabikhan407/ZOHAIB_VPS_SCRIPT/main"
 
-# 1. Update & Tools (Visual)
-echo "ZOHAIB_NETWORK: Starting Installation..."
+# 1. Server Preparation
+echo -e "${C}ZOHAIB_NETWORK: Installing Core Engines...${NC}"
 apt update && apt upgrade -y
-apt install jq curl socat xz-utils wget net-tools -y
+apt install jq curl socat xz-utils wget net-tools python3 nginx certbot python3-certbot-nginx -y
 
-# 2. Xray & Webserver
+# 2. Xray Core & SSH WS Setup
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+mkdir -p /etc/zohaib/
+mkdir -p /var/log/xray/
 
-# 3. Download Menu
-wget -O /usr/bin/menu "${REPO_URL}/menu.sh" && chmod +x /usr/bin/menu
+# 3. Download All Functional Scripts
+echo -e "${C}ZOHAIB_NETWORK: Downloading Functions...${NC}"
+files=("menu.sh" "ssh-menu.sh" "xray-menu.sh" "add-vless.sh" "add-ssh.sh" "check-online.sh")
+for file in "${files[@]}"; do
+    wget -O /usr/bin/${file%.sh} "${REPO_URL}/${file}"
+    chmod +x /usr/bin/${file%.sh}
+done
 
-# Yahan hum bad mein aur files add karenge jise add-ssh, add-vless wagera
+# 4. Domain SSL Setup (Placeholder - User will run manually from menu)
 clear
-echo "ZOHAIB_NETWORK: Installation Mukammal!"
-echo "Type 'menu' to start."
+echo -e "${P}==================================================${NC}"
+echo -e " ${G}INSTALLATION MUKAMMAL!${NC}"
+echo -e " ${C}Domain point hai to ab 'menu' likhein.${NC}"
+echo -e "${P}==================================================${NC}"
