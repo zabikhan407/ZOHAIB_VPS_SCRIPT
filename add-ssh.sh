2415b313ea6ce6d1d22cfc0cb4aeb464dacdb0f5
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 01:08:50 +0500

    Update add-ssh.sh

--- add-ssh.sh
+++ add-ssh.sh
@@ -2,12 +2,10 @@
 clear
 read -p "Username : " user
 read -p "Password : " pass
-read -p "Days     : " days
-
+read -p "Duration (Days) : " days
 exp=`date -d "$days days" +"%Y-%m-%d"`
 useradd -e $exp -M -s /bin/false $user
 echo "$user:$pass" | chpasswd
-
-host=$(curl -s ipinfo.io/ip)
-echo -e "SSH Account Created!"
-echo -e "Host: $host\nPort: 80, 8880 (WS)\nUser: $user\nPass: $pass\nExp: $exp"
+echo "SSH Account $user Created! Expires: $exp"
+sleep 3
+ssh-menu
