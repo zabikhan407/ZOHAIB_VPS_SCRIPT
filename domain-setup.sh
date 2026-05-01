6b6639a4b4729292ffe71a72af3f7f6570d84eb5
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 02:37:01 +0500

    Update domain-setup.sh

--- domain-setup.sh
+++ domain-setup.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 clear
-read -p "Apna Domain Likhein (e.g. zohaib.com): " domain
+echo -e "DOMAIN & SSL AUTO-CONFIG"
+read -p "Apna Pointed Domain Likhein: " domain
 echo "$domain" > /etc/xray/domain
 
-# SSL Certificate hasil karna
+# SSL Certificate install karna
+systemctl stop nginx
 certbot certonly --standalone --preferred-challenges http -d $domain --register-unsafely-without-email --agree-tos
+systemctl start nginx
 
-# Nginx config generate karna
-cat <<EOF > /etc/nginx/sites-available/xray
-server {
-    listen 80;
-    server_name $domain;
-    location /vless {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:10001;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-    }
-}
-EOF
-ln -s /etc/nginx/sites-available/xray /etc/nginx/sites-enabled/
-systemctl restart nginx
-echo "Domain Setup & SSL Mukammal!"
+# Xray and Nginx Linking
+echo "SSL installed successfully on $domain"
 sleep 3 ; menu
