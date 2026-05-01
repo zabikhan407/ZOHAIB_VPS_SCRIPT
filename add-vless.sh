2cb63aa4989cc36fee91d445777247726abbdf17
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 01:21:59 +0500

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 clear
 read -p "Username : " user
-read -p "Days : " days
+read -p "Duration (Days) : " days
+read -p "Port (Default 443) : " port
+port=${port:-443}
+read -p "TLS Enabled? (y/n) : " tls_choice
+
 uuid=$(cat /proc/sys/kernel/random/uuid)
 exp=`date -d "$days days" +"%Y-%m-%d"`
-domain=$(curl -s ipinfo.io/ip)
-echo -e "VLESS Created!\nUUID: $uuid\nExp: $exp"
-echo -e "Link: vless://$uuid@$domain:443?path=/vless&security=none&encryption=none&type=ws#$user"
-sleep 5
-xray-menu
+domain=$(cat /etc/xray/domain) # Domain setup se ayega
+
+if [[ "$tls_choice" == "y" ]]; then
+    link="vless://$uuid@$domain:$port?path=/vless&security=tls&encryption=none&type=ws#$user"
+else
+    link="vless://$uuid@$domain:$port?path=/vless&security=none&encryption=none&type=ws#$user"
+fi
+
+echo -e "VLESS ACCOUNT CREATED\nUser: $user\nUUID: $uuid\nPort: $port\nExp: $exp"
+echo -e "Link: $link"
+sleep 5 ; xray-menu
