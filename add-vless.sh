18c61ce65dd248b371bda64b4943b7c4fa6c5ce2
zabikhan407 <zabiiihacker670@gmail.com>
2026-05-02 10:11:12 +0500

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 clear
-read -p "Username : " user
-read -p "Duration (Days) : " days
-read -p "Port (Default 443) : " port
-port=${port:-443}
-read -p "TLS Enabled? (y/n) : " tls_choice
-
-uuid=$(cat /proc/sys/kernel/random/uuid)
-exp=`date -d "$days days" +"%Y-%m-%d"`
-domain=$(cat /etc/xray/domain) # Domain setup se ayega
-
-if [[ "$tls_choice" == "y" ]]; then
-    link="vless://$uuid@$domain:$port?path=/vless&security=tls&encryption=none&type=ws#$user"
-else
-    link="vless://$uuid@$domain:$port?path=/vless&security=none&encryption=none&type=ws#$user"
-fi
-
-echo -e "VLESS ACCOUNT CREATED\nUser: $user\nUUID: $uuid\nPort: $port\nExp: $exp"
-echo -e "Link: $link"
-sleep 5 ; xray-menu
+# ... (Banner aur Input wahi rahega) ...
+# Link generate karne ke baad ye add karein:
+echo -e "${G}Link TLS :${NC} vless://$uuid@$domain:443?path=/vless&security=tls&encryption=none&type=ws#$user"
+echo -e "${G}Link NTLS:${NC} vless://$uuid@$domain:80?path=/vless&security=none&encryption=none&type=ws#$user"
+echo -e "----------------------------------------------"
+echo -e "${YL}Copy kar lein. Wapis janay ke liye Enter dabayein...${NC}"
+read # Ye line screen ko rok kar rakhegi
+xray-menu
